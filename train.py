c3c77d146592b318b0488ff17bc9877f4af1c516
jene4ekjene4ek <32958504+jene4ekjene4ek@users.noreply.github.com>
2019-10-16 12:21:47 +0300

    Update train.py

--- train.py
+++ train.py
@@ -62,25 +62,25 @@ def train(data_path, max_depth, max_bins):
     spark.stop()
     
     
-    if __name__ == "__main__":
-      from argparse import ArgumentParser
-      parser = ArgumentParser()
-      parser.add_argument("--experiment_name", dest="experiment_name", help="experiment_name", default="pyspark", required=False)
-      parser.add_argument("--data_path", dest="data_path", help="data_path", required=True)
-      parser.add_argument("--max_depth", dest="max_depth", help="max_depth", default=2, type=int)
-      parser.add_argument("--max_bins", dest="max_bins", help="max_bins", default=32, type=int)
-      parser.add_argument("--describe", dest="describe", help="Describe data", default=False, action='store_true')
-      args = parser.parse_args()
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--experiment_name", dest="experiment_name", help="experiment_name", default="pyspark", required=False)
+    parser.add_argument("--data_path", dest="data_path", help="data_path", required=True)
+    parser.add_argument("--max_depth", dest="max_depth", help="max_depth", default=2, type=int)
+    parser.add_argument("--max_bins", dest="max_bins", help="max_bins", default=32, type=int)
+    parser.add_argument("--describe", dest="describe", help="Describe data", default=False, action='store_true')
+    args = parser.parse_args()
 
-      client = mlflow.tracking.MlflowClient()
-      print("experiment_name:",args.experiment_name)
-      mlflow.set_experiment(args.experiment_name)
-      print("experiment_id:",client.get_experiment_by_name(args.experiment_name).experiment_id)
+    client = mlflow.tracking.MlflowClient()
+    print("experiment_name:",args.experiment_name)
+    mlflow.set_experiment(args.experiment_name)
+    print("experiment_id:",client.get_experiment_by_name(args.experiment_name).experiment_id)
 
-      
-      with mlflow.start_run() as run:
-          print("MLflow:")
-          print("  run_id:",run.info.run_uuid)
-          print("  experiment_id:",run.info.experiment_id)
-          train(args.data_path, args.max_depth, args.max_bins)
+
+    with mlflow.start_run() as run:
+      print("MLflow:")
+      print("  run_id:",run.info.run_uuid)
+      print("  experiment_id:",run.info.experiment_id)
+      train(args.data_path, args.max_depth, args.max_bins)
       
