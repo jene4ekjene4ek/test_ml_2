16b519b8d187aa7e1e255846f4ddf36c28280b80
jene4ekjene4ek <32958504+jene4ekjene4ek@users.noreply.github.com>
2019-10-21 15:51:31 +0300

    Update train.py

--- train.py
+++ train.py
@@ -16,7 +16,8 @@ from pyspark import SparkConf
 from pyspark import SparkContext
 
 
-def train(sc, endpoint, access_key, secret_key, data_path, max_depth, max_bins):
+# def train(sc, endpoint, access_key, secret_key, data_path, max_depth, max_bins):
+def train(sc, data_path, max_depth, max_bins):
     print("Parameters: max_depth: {}  max_bins: {}".format(max_depth,max_bins))
 #     config = SparkConf().setAll(
 #                                 [('spark.hadoop.fs.s3a.endpoint', endpoint), 
