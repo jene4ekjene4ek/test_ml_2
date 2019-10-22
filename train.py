404a04da45f185c625571918c66662367dffe62d
jene4ekjene4ek <32958504+jene4ekjene4ek@users.noreply.github.com>
2019-10-22 10:08:23 +0300

    Update train.py

--- train.py
+++ train.py
@@ -78,6 +78,7 @@ def train(sc, endpoint, access_key, secret_key, data_path, max_depth, max_bins, 
     
     
     mlflow.spark.log_model(model, '')
+    mlflow.spark.save_model(model, '')
 #     try:
 #     mlflow.spark.save_model(model, save_path)
 #     except: 
