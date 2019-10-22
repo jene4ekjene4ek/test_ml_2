f10ef519ae89d4043385e89bffe9794862b60269
jene4ekjene4ek <32958504+jene4ekjene4ek@users.noreply.github.com>
2019-10-22 13:19:07 +0300

    Update train.py

--- train.py
+++ train.py
@@ -105,8 +105,8 @@ if __name__ == "__main__":
     conf = SparkConf().setMaster("local").setAppName("DegreesOfSeparation")
     sc = SparkContext(conf=conf)
     
-    #mlflow.create_experiment('exp', artifact_location="s3://orlow-cos/pyspark-model/")
-#     mlflow.set_experiment('exp')
+    mlflow.create_experiment('exp', artifact_location="s3://orlow-cos/pyspark-model/")
+    mlflow.set_experiment('exp')
     
     with mlflow.start_run() as run:
       print("MLflow:")
