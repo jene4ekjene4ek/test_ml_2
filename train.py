920807a8e24c9aea8fa0259e0a17b3b7d6592334
jene4ekjene4ek <32958504+jene4ekjene4ek@users.noreply.github.com>
2019-10-22 10:47:19 +0300

    Update train.py

--- train.py
+++ train.py
@@ -114,5 +114,5 @@ if __name__ == "__main__":
       print("  experiment_id:",run.info.experiment_id)
       train(sc, args.endpoint, args.access_key, args.secret_key, str(args.data_path), args.max_depth, args.max_bins, str(args.save_path))
     mlflow.get_artifact_uri(artifact_path=None)
-    mlflow.log_artifact(artifact_path=None)
+#     mlflow.log_artifact(artifact_path=None)
     print(path)
