e5051d76970af869a7c9a3e6aa1e43fd7a7bdcbc
jene4ekjene4ek <32958504+jene4ekjene4ek@users.noreply.github.com>
2019-11-07 17:54:17 +0200

    Update train.py

--- train.py
+++ train.py
@@ -34,8 +34,8 @@ def train(sc, endpoint, access_key, secret_key, data_path, max_depth, max_bins):
     # Load the data stored in LIBSVM format as a DataFrame.
     data = spark.read.format("libsvm").load(data_path)
     
-    df_l = data.select('label')
-    df_l.repartition(1).write.csv('s3://orlow-cos/pyspark-model/data.csv', mode='overwrite', sep='\t', header=True)
+#     df_l = data.select('label')
+#     df_l.repartition(1).write.csv('s3://orlow-cos/pyspark-model/data.csv', mode='overwrite', sep='\t', header=True)
 
     # Index labels, adding metadata to the label column.
     # Fit on whole dataset to include all labels in index.
